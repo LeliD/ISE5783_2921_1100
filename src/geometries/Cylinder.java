4e42f01cffb8b4b0fbb57337cde8c50094b3811a
Shilat Sharon <shisharo@g.jct.ac.il>
2023-04-17 18:12:46 +0300

    17/04

--- src/geometries/Cylinder.java
+++ src/geometries/Cylinder.java
@@ -1,9 +1,11 @@
 package geometries;
 
+import static primitives.Util.isZero;
+
 import primitives.Point;
 import primitives.Ray;
 import primitives.Vector;
-import static primitives.Util.isZero;
+
 /**
  * Cylinder class for representing a Cylinder, extends Tube
  * 
@@ -37,20 +39,21 @@ public class Cylinder extends Tube {
 	}
 
 	@Override
-	public Vector getNormal(Point p) 
-	{
-		if(p.equals(axisRay.getP0()))//if point p is on the center of the cylinder's first base
+	public Vector getNormal(Point p) {
+		if (p.equals(axisRay.getP0()))// if point p is on the center of the cylinder's first base
 			return axisRay.getDir().scale(-1);
-		Point centerSecondBase=axisRay.getP0().add(axisRay.getDir().scale(height));
-		if(p.equals(centerSecondBase))//if point p is on the center of the cylinder's upper base
+		Point centerSecondBase = axisRay.getP0().add(axisRay.getDir().scale(height));
+		if (p.equals(centerSecondBase))// if point p is on the center of the cylinder's upper base
 			return axisRay.getDir();
-		Vector v1=p.subtract(axisRay.getP0());//p-p0
-		if(v1.length()<radius && isZero(v1.dotProduct(axisRay.getDir())))//if point p is on the cylinder's first base
+		Vector v1 = p.subtract(axisRay.getP0());// p-p0
+		if (v1.length() < radius && isZero(v1.dotProduct(axisRay.getDir())))// if point p is on the cylinder's first
+																			// base
 			return axisRay.getDir().scale(-1);
-		Vector v2=p.subtract(centerSecondBase);//p-centerSecondBase
-		if(v2.length()<radius && isZero(v2.dotProduct(axisRay.getDir())))//if point p is on the cylinder's upper base
+		Vector v2 = p.subtract(centerSecondBase);// p-centerSecondBase
+		if (v2.length() < radius && isZero(v2.dotProduct(axisRay.getDir())))// if point p is on the cylinder's upper
+																			// base
 			return axisRay.getDir();
-		//in case point p is on the cylinder's side
-		return super.getNormal(p);//
+		// in case point p is on the cylinder's side
+		return super.getNormal(p);
 	}
 }
