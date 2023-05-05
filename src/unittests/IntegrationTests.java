6f8a849fa5207b4c7143916b83d259c530e520a0
Shilat Sharon <shisharo@g.jct.ac.il>
2023-05-05 09:43:47 +0300

    bvc

---
+++ src/unittests/IntegrationTests.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package unittests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Shilat Sharon
+ *
+ */
+class IntegrationTests {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
